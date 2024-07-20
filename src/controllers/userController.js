60d9122dcedb9f4e132be5479b97cdd822ca9569
JPRM242 <josuemadingou5@gmail.com>
2024-07-20 16:43:25 +0000

    deployement des nouveaux fichier JS

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -31,7 +31,7 @@ const createUser = async (req, res) => {
 
         const mailOptions = {
           from: 'josuemadingou5@gmail.com',
-          to: 'emmanuelnzaou@gmail.com',
+          to: 'madingoujosuepr@gmail.com',
           subject: 'Utilisateur enregistré',
           html: emailContent
         }
